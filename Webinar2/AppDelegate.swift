b1f3d7de4d285305d04450f8c60b5b75c04861ce
joseluissanchez-porrogodoy_portatil <joseluis.sanchezporro.godoy@gmail.com>
2019-02-24 11:36:20 +0100

    Delete main.storyboard and use ViewController

--- Webinar2/AppDelegate.swift
+++ Webinar2/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let viewController = FirstViewController()
+        viewController.title = "Primera Vista"
+        let nav = UINavigationController(rootViewController: viewController)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
