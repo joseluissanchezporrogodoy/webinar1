faf1625eea7942012dcc30523390f2e8e990073b
joseluissanchez-porrogodoy_portatil <joseluis.sanchezporro.godoy@gmail.com>
2019-02-24 11:48:59 +0100

    Add views to FirstViewController and IBOulets and IBActions

---
+++ Webinar2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Webinar2
+//
+//  Created by jose luis sanchez-porro godoy on 24/02/2019.
+//  Copyright © 2019 uax. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
